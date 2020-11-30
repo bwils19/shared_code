d7d1494fb97e547fd923121fbd7f0d5e8fc4a88d
bwils19 <bwils19@users.noreply.github.com>
2020-11-30 02:54:03 +0000

    automated commit by submit50

--- readability.c
+++ readability.c
@@ -6,10 +6,13 @@
 
 int main(void)
 {
+    // Get the text from the user
     string s = get_string("Text: ");
     int lett = 0; 
     int word = 0;
     int sent = 0;
+    
+    // Count letters, words anbd sentences
     for (int i = 0; s[i] != '\0'; i++)
     {
         if (isalpha(s[i]))
@@ -26,7 +29,9 @@ int main(void)
         }
     }
     word++;
-    int grade = round(0.0588 * (100*lett/ (float) word) - 0.296 * (100 * sent / (float) word)  - 15.8);
+    
+    // Calculate the Coleman-Liau index
+    int grade = round(0.0588 * (100 * lett / (float) word) - 0.296 * (100 * sent / (float) word)  - 15.8);
     if (grade < 1)
     {
         printf("Before Grade 1");
