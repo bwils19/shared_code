78acc209b64d100c4570d5934d894a82c4710861
bwils19 <bwils19@users.noreply.github.com>
2020-12-15 20:37:47 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -42,19 +42,19 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
                 int originalGreen = image[i][j].rgbtGreen;
                 
                 // Get the sepia value for each color scale for each pixel
-                float sepiaRed = round(.393 * originalRed + .769 * originalGreen + .189 * originalBlue);
+                int sepiaRed = round(.393 * originalRed + .769 * originalGreen + .189 * originalBlue);
                 if (sepiaRed > 255)
                 {
                     sepiaRed = 255;
                 }
                 
-                float sepiaGreen = round(.349 * originalRed + .686 * originalGreen + .168 * originalBlue);
+                int sepiaGreen = round(.349 * originalRed + .686 * originalGreen + .168 * originalBlue);
                 if (sepiaGreen > 255)
                 {
                     sepiaGreen = 255;
                 }
                 
-                float sepiaBlue = round(.272 * originalRed + .534 * originalGreen + .131 * originalBlue);
+                int sepiaBlue = round(.272 * originalRed + .534 * originalGreen + .131 * originalBlue);
                 if (sepiaBlue > 255)
                 {
                     sepiaBlue = 255;
